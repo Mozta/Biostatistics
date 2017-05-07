1ceee91c6fb7597da9d3781c61b92275cbbddece
Kleity <perezaguirrejazmin@gmail.com>
2017-05-07 14:33:03 -0500

    Add image plot

--- script1.py
+++ script1.py
@@ -6,7 +6,7 @@ with open('dataset_anos.txt', 'rt') as reader:
     for point in reader:
         X.append(np.asarray(point.split(",")))
 
-colors = ["r.", "g.", "b."]
+colors = ["r."]
 for i in range(len(X)):
     plt.plot(X[i][0], X[i][1], colors[0])
 plt.show()
