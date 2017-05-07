c75909abbe4387341142a4394433ac87853370ea
Kleity <perezaguirrejazmin@gmail.com>
2017-05-07 15:47:36 -0500

    Script to calculate arithmetic average

--- script0.py
+++ script0.py
@@ -1,16 +1,23 @@
-import numpy as np
+#Media aritmética
 import matplotlib.pyplot as plt
 
-X = list()
-with open('dataset_anos.txt', 'rt') as reader:
-    for point in reader:
-        X.append(np.asarray(map(float, point.split(","))))
-
-print(X)
-print(len(X))
+x = list()
+y = list()
 sum = 0
 
-for i in X:
-    sum =+ X[1]
+#Lee el archivo de texto
+with open('dataset_anos.txt', 'rt') as reader:
+    #Recorre el archivo de texto líne a línea
+    for point in reader:
+        #Separa la cadena en elementos y se toma el elemento en la posición 0 para guardarlo en x
+        x.append(int(point.split(",")[0]))
+        #Separa la cadena en elementos y se toma el elemento en la posición 1 para guardarlo en y
+        y.append(float(point.split(",")[1]))
+#Media
+for i in x:
+    sum =+ x[1]
+
+media = (1/float(len(x))) * sum
 
 print "El total es = " + str(sum)
+print "La media es = " + str(media)
